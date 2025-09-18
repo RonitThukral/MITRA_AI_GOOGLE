c8675b88fc35bd1dd5a94871c652801b9a3ca5b3
Ronit <sarikaarora3397@gmail.com>
2025-09-18 14:46:46 +0530

    uncommented uvicorn run block

--- main.py
+++ main.py
@@ -280,9 +280,9 @@ async def health_check():
         }
     }
 
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 8080))
-#     uvicorn.run(app, host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 
 

86e67e0c6d9fa484ec972655b37ab759a53a8cd8
Ronit <sarikaarora3397@gmail.com>
2025-09-18 14:27:36 +0530

    removed local credentials file

--- config.py
+++ config.py
@@ -12,7 +12,6 @@ PROMPTS_PATH = Path("prompts/system_prompts.json")
 CRISIS_LOG = Path("crisis_log.jsonl")
 
 # Credentials
-# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\hp\Desktop\chatapp\service.json"
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 
